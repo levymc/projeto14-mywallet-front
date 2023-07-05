7982decd081a2467cb5adc3eb6884e0bf5604b83
Levy <levymcruz@gmail.com>
2023-07-05 16:32:12 -0300

    att

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,22 +1,44 @@
-import styled from "styled-components"
-import { Link } from "react-router-dom"
-import MyWalletLogo from "../components/MyWalletLogo"
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import MyWalletLogo from "../components/MyWalletLogo";
+import axios from "axios";
 
 export default function SignInPage() {
+  // Função para manipular o envio do formulário
+  const handleRequest = async (event) => {
+    event.preventDefault(); // Impede o comportamento padrão de envio do formulário
+    const email = event.target.elements.email.value; // Obtém o valor do campo de e-mail
+    const senha = event.target.elements.senha.value; // Obtém o valor do campo de senha
+
+    try {
+      // Realiza a requisição POST para a rota "/login" utilizando o axios
+      const response = await axios.post("http://localhost:5000/login", {
+        email: email,
+        senha: senha,
+      });
+      console.log(response.data); // Exibe a resposta do servidor no console
+    } catch (error) {
+      console.error(error); // Exibe o erro no console, caso ocorra algum problema na requisição
+    }
+  };
+
   return (
     <SingInContainer>
-      <form>
+      <form onSubmit={handleRequest}>
         <MyWalletLogo />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <button>Entrar</button>
+        <input placeholder="E-mail" type="email" name="email" />
+        <input
+          placeholder="Senha"
+          type="password"
+          name="senha"
+          autoComplete="new-password"
+        />
+        <button type="submit">Entrar</button>
       </form>
 
-      <Link>
-        Primeira vez? Cadastre-se!
-      </Link>
+      <Link to="/cadastro">Primeira vez? Cadastre-se!</Link>
     </SingInContainer>
-  )
+  );
 }
 
 const SingInContainer = styled.section`
@@ -25,4 +47,7 @@ const SingInContainer = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`
+`;
+
+
+
