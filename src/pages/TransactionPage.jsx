077529a0b6b4d6c790ba3753e677ab3fef291284
Levy <levymcruz@gmail.com>
2023-07-10 12:02:13 -0300

    VITE_API_URL_DEV -> VITE_API_URL

--- src/pages/TransactionPage.jsx
+++ src/pages/TransactionPage.jsx
@@ -1,39 +1,85 @@
-import styled from "styled-components"
-import { useLocation, useParams } from "react-router-dom";
-import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+
 
 export default function TransactionsPage() {
-  const { tipo } = useParams();
-
-  const navigateTo = useNavigate();
-
+    const { tipo } = useParams();
+    const navigateTo = useNavigate();
+    const userData = JSON.parse(localStorage.getItem("userData"));
+    console.log(userData.userId, userData.token)
     const [formData, setFormData] = useState({
-        email: "",
-        senha: "",
+        valor: "",
+        descricao: "",
+        type: tipo
     });
 
-  return (
-    <TransactionsContainer>
-      <h1>Nova {tipo}</h1>
-      <form>
-        <input data-test="registry-amount-input" placeholder="Valor" type="text"/>
-        <input data-test="registry-name-input" placeholder="Descrição" type="text" />
-        <button data-test="registry-save">Salvar TRANSAÇÃO</button>
-      </form>
-    </TransactionsContainer>
-  )
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData((prevState) => ({
+        ...prevState,
+        [name]: value,
+        }));
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const url = tipo === "entrada" ? import.meta.env.VITE_API_URL + "/entrada" : import.meta.env.VITE_API_URL + "/saida";
+        const token = {
+            headers: {
+                Authorization: `Bearer ${userData.token}`,
+            },
+        };
+      
+        axios.post(url, formData, token).then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => {
+                alert(error.response.data);
+                console.error(error.response.data);
+            });
+      };
+      
+    
+
+    return (
+        <TransactionsContainer>
+            <h1>Nova {tipo}</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                data-test="registry-amount-input"
+                placeholder="Valor"
+                type="number"
+                name="valor"
+                value={formData.valor}
+                onChange={handleChange}
+                />
+                <input
+                data-test="registry-name-input"
+                placeholder="Descrição"
+                type="text"
+                name="descricao"
+                value={formData.descricao}
+                onChange={handleChange}
+                />
+                <button data-test="registry-save" type="submit">
+                Salvar TRANSAÇÃO
+                </button>
+            </form>
+        </TransactionsContainer>
+    );
 }
 
 const TransactionsContainer = styled.main`
-  height: calc(100vh - 50px);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
+    height: calc(100vh - 50px);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
 
-  h1 {
-    align-self: flex-start;
-    margin-bottom: 40px;
-  }
-`
+    h1 {
+        align-self: flex-start;
+        margin-bottom: 40px;
+    }
+`;
