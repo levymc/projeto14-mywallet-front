3844ad4df4ca7390aeac025db8f7779933e0fb78
Levy <levymcruz@gmail.com>
2023-07-10 17:22:53 -0300

    fixing fixed2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,7 +8,6 @@ import axios from "axios";
 export default function HomePage() {
     const [transactions, setTransactions] = useState([]);
     const userData = JSON.parse(localStorage.getItem("userData"));
-    console.log(userData)
     useEffect(() => {
         const getTransactions = async () => {
             try {
@@ -42,7 +41,7 @@ export default function HomePage() {
             {transactions.map((transaction) => (
                 <ListItemContainer key={transaction._id}>
                 <div>
-                    <span>Eu {transaction.valor}</span>
+                    <span>{transaction.data}</span>
                     <strong data-test="registry-name">{transaction.descricao}</strong>
                 </div>
                 <Value
@@ -83,70 +82,70 @@ export default function HomePage() {
 
 
 const HomeContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: calc(100vh - 50px);
+    display: flex;
+    flex-direction: column;
+    height: calc(100vh - 50px);
 `
 const Header = styled.header`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0 2px 5px 2px;
-  margin-bottom: 15px;
-  font-size: 26px;
-  color: white;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 2px 5px 2px;
+    margin-bottom: 15px;
+    font-size: 26px;
+    color: white;
 `
 const TransactionsContainer = styled.article`
-  flex-grow: 1;
-  background-color: #fff;
-  color: #000;
-  border-radius: 5px;
-  padding: 16px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  article {
-    display: flex;
-    justify-content: space-between;   
-    strong {
-      font-weight: 700;
-      text-transform: uppercase;
-    }
-  }
-`
-const ButtonsContainer = styled.section`
-  margin-top: 15px;
-  margin-bottom: 0;
-  display: flex;
-  gap: 15px;
-  
-  Link button {
-    width: 50%;
-    height: 115px;
-    font-size: 22px;
-    text-align: left;
+    flex-grow: 1;
+    background-color: #fff;
+    color: #000;
+    border-radius: 5px;
+    padding: 16px;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    p {
-      font-size: 18px;
+    article {
+        display: flex;
+        justify-content: space-between;   
+        strong {
+        font-weight: 700;
+        text-transform: uppercase;
+        }
     }
-  }
+`
+const ButtonsContainer = styled.section`
+    margin-top: 15px;
+    margin-bottom: 0;
+    display: flex;
+    gap: 15px;
+    
+    Link button {
+        width: 50%;
+        height: 115px;
+        font-size: 22px;
+        text-align: left;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        p {
+        font-size: 18px;
+        }
+    }
 `
 const Value = styled.div`
-  font-size: 16px;
-  text-align: right;
-  color: ${(props) => (props.color === "positivo" ? "green" : "red")};
+    font-size: 16px;
+    text-align: right;
+    color: ${(props) => (props.color === "positivo" ? "green" : "red")};
 `
 const ListItemContainer = styled.li`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 8px;
-  color: #000000;
-  margin-right: 10px;
-  div span {
-    color: #c6c6c6;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 8px;
+    color: #000000;
     margin-right: 10px;
-  }
+    div span {
+        color: #c6c6c6;
+        margin-right: 10px;
+    }
 `
--- src/pages/TransactionPage.jsx
+++ src/pages/TransactionPage.jsx
@@ -4,84 +4,90 @@ import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 export default function TransactionsPage() {
-  const { tipo } = useParams();
-  const navigateTo = useNavigate();
-  const userData = JSON.parse(localStorage.getItem("userData"));
+    const { tipo } = useParams();
+    const navigateTo = useNavigate();
+    const userData = JSON.parse(localStorage.getItem("userData"));
 
-  const [formData, setFormData] = useState({
-    valor: "",
-    descricao: "",
-    type: tipo
-  });
+    const [formData, setFormData] = useState({
+        valor: "",
+        descricao: "",
+        type: tipo
+    });
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormData((prevState) => ({
-      ...prevState,
-      [name]: value
-    }));
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const url = import.meta.env.VITE_API_URL_DEV + "/transactions";
-    const token = {
-      headers: {
-        Authorization: `Bearer ${userData.token}`,
-        id: userData.userId
-      }
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData((prevState) => ({
+        ...prevState,
+        [name]: value
+        }));
     };
-    console.log(formData)
-    axios
-      .post(url, formData, token)
-      .then((response) => {
-        console.log(response.data);
-        navigateTo("/home");
-      })
-      .catch((error) => {
-        alert(error.response.data);
-        console.error(error.response.data);
-      });
-  };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const formattedValue = parseFloat(formData.valor).toFixed(2);
+
+        const formattedData = {
+        ...formData,
+        valor: formattedValue
+        };
+
+        const url = import.meta.env.VITE_API_URL_DEV + "/transactions";
+        const token = {
+        headers: {
+            Authorization: `Bearer ${userData.token}`,
+            id: userData.userId
+        }
+        };
+
+        axios.post(url, formattedData, token).then((response) => {
+            console.log(response.data);
+            navigateTo("/home");
+        })
+        .catch((error) => {
+            alert(error.response.data);
+            console.error(error.response.data);
+        });
+    };
 
   return (
     <TransactionsContainer>
-      <h1>Nova {tipo}</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          data-test="registry-amount-input"
-          placeholder="Valor"
-          type="number"
-          name="valor"
-          value={formData.valor}
-          onChange={handleChange}
-          step="0.01"
-        />
-        <input
-          data-test="registry-name-input"
-          placeholder="Descrição"
-          type="text"
-          name="descricao"
-          value={formData.descricao}
-          onChange={handleChange}
-        />
-        <button data-test="registry-save" type="submit">
-          Salvar TRANSAÇÃO
-        </button>
-      </form>
+        <h1>Nova {tipo}</h1>
+        <form onSubmit={handleSubmit}>
+            <input
+            data-test="registry-amount-input"
+            placeholder="Valor"
+            type="number"
+            name="valor"
+            value={formData.valor}
+            onChange={handleChange}
+            step="0.01"
+            />
+            <input
+            data-test="registry-name-input"
+            placeholder="Descrição"
+            type="text"
+            name="descricao"
+            value={formData.descricao}
+            onChange={handleChange}
+            />
+            <button data-test="registry-save" type="submit">
+            Salvar TRANSAÇÃO
+            </button>
+        </form>
     </TransactionsContainer>
   );
 }
 
 const TransactionsContainer = styled.main`
-  height: calc(100vh - 50px);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
+    height: calc(100vh - 50px);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
 
-  h1 {
-    align-self: flex-start;
-    margin-bottom: 40px;
-  }
-`;
+    h1 {
+        align-self: flex-start;
+        margin-bottom: 40px;
+    }
+`
